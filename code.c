c57e25b10c461ae7100520c5701c606db39507df
DibaHadiE <diba.hadie@gmail.com>
2022-12-02 11:57:42 +0330

    v3-main

--- code.c
+++ code.c
@@ -4,4 +4,8 @@ int main(){
     printf("Hello World!");
     printf("\nThis is a new branch");
     printf("\nMerging the changes");
+    int a, b;
+    a = 2;
+    b = 2;
+    printf("a+b = %d", a+b);
 }
